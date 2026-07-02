58716db09e2056e87eb2a5723a6134cf2a17c773
sunquan819 <sunquan819@gmail.com>
2026-07-02 09:26:50 +0800

    fix: store RDP credentials for actual target host, not 127.0.0.1

--- pkg/rdp/agent_windows.go
+++ pkg/rdp/agent_windows.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func launch(target, username, password string) error {
-	storeCredentials("127.0.0.1", username, password)
+	host := target
+	if idx := strings.LastIndex(target, ":"); idx > 0 {
+		host = target[:idx]
+	}
+	storeCredentials(host, username, password)
 	rdpFile := os.Getenv("TEMP") + "\\rdp_connect.rdp"
 	content := fmt.Sprintf(
 		"full address:s:%s\r\nusername:s:%s\r\nprompt for credentials:i:0\r\nauthentication level:i:2\r\n",
